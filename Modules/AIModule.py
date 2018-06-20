6d9d7dabd119f1d097de0f31e97209a6cdd26eb3
rodrigo.moreira206@gmail.com <S3nhagit>
2018-06-20 10:32:57 -0300

    SVM RBF - Iris data-set comparisson

--- Modules/AIModule.py
+++ Modules/AIModule.py
@@ -51,6 +51,10 @@ def nmae(y_real, y_predict):
 
     return nmae_resultado
 
+#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#
+#Test using Logistic Regression
+#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#
+
 
 #Preparing X and Y to be trained
 df = pd.read_csv('/home/rodrigo/MPLS-TE/Data-Set/cic-unb/merged_5s.csv')
@@ -61,9 +65,10 @@ x = df.iloc[:,0:28]
 x = x.iloc[:,x.columns != "Source_IP"]
 x = x.iloc[:,x.columns != "Destination_IP"]
 
-
+#To catch target (class of network traffic)
 y = df.iloc[:,29:30]
 
+#Data-set train test split
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.30)
 
 #y_train must be 1d column
@@ -80,14 +85,10 @@ C.fit(x_train,y_train)
 y_pred = C.predict(x_test)
 
 # y_pred_class = pd.DataFrame(y_pred_class)
-print("Accuracy of the Classifier C = %.3f" % metrics.accuracy_score(y_test, y_pred))
-
-#Print Coeficients of Regression
-#print("Coeficients: " + np.array2string(C.coef_))
-
+print("Logistic Regression: Accuracy of the Classifier C = %.3f" % metrics.accuracy_score(y_test, y_pred))
 
 #Regression Method
-print("The Normalized Mean Absolute Error (Regression Method): %0.4f " % nmae(y_test, y_pred))
+print("Logistic Regression: Normalized Mean Absolute Error (NMAE): %0.4f " % nmae(y_test, y_pred))
 
 
 
@@ -95,9 +96,10 @@ print("The Normalized Mean Absolute Error (Regression Method): %0.4f " % nmae(y_
 #Test using IRIS data set
 #-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#
 
-
+#Load IRIS dataset 
 iris = datasets.load_iris()
-print(iris)
+
+#Catch only 2 parameters (there are two)
 X = iris.data[:, :2]
 y = iris.target
 
@@ -110,52 +112,54 @@ xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
 
 X_plot = np.c_[xx.ravel(), yy.ravel()]
 
-print(X)
-print(y)
-print(X.shape)
-print(y.shape)
-
-
 # Create the SVC model object
 C = 1.0 # SVM regularization parameter
 svc = svm.SVC(kernel='linear', C=C, decision_function_shape='ovr').fit(X, y)
 Z = svc.predict(X_plot)
-print("Z Predicted")
-print(Z)
+
+#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#
+#End of IRIS data-set comparisson
+#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#
+
+
 
 
 #-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#
-#
+#IRIS Data-set versus Network Data-set (both has same features)
 #-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#
 
-#Preparing X and Y to be trained
+#Reading Data to be trained
 df = pd.read_csv('/home/rodrigo/MPLS-TE/Data-Set/cic-unb/test.csv')
+
+#Cacth features from Test - In this case only two (to be compared with IRIS Data-set)
 X = df.iloc[:,8:10]
 y = df.iloc[:,29:30]
 
-
+#Split data in Train and Test
 x_train, x_test, y_train, y_test = train_test_split(X,y, test_size=0.30)
 
-
+#Conversion into numpy array
 x_train = np.array(x_train)
 y_train = np.array(y_train)
 y_train = column_or_1d(y_train, warn=False)
 
-print("Comparativo")
-print(X.dtypes)
+#Adjustments in numpy to keep big number format (without scientific notation)
 np.set_printoptions(suppress=False,formatter={'float_kind':'{:16.5f}'.format},linewidth=130)
-print(np.array(x_train))
-#print(y_train)
-#print(x_train.shape)
-#print(y_train.shape)
 
 # Create the SVC model object
 C = 1.0 # SVM regularization parameter
 svc = svm.SVC(kernel='rbf', C=C, decision_function_shape='ovr').fit(x_train, y_train)
 
+#Test using splitted data
 Z = svc.predict(x_test)
-print("Z Predicted2:")
-print(Z)
+
+#Print Accuracy of the Classifier (SVM)
+print("SVM: Accuracy of the Classifier C = %.3f" % metrics.accuracy_score(y_test, Z))
+
+#Print NAME Error measure (SVM)
+print("SVM: Normalized Mean Absolute Error (NMAE): %0.4f " % nmae(y_test, Z))
 
 
-
+#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#
+#End of IRIS and Network Data-set compatisson
+#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#-------------#
